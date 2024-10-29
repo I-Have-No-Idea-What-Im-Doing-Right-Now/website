c9399c3528a4d67533c1c09d9c64ba83ee7f8c88
Leo <leo@iMac.local>
2024-10-28 19:41:23 -0500

    Fixes for page

--- src/Title.js
+++ src/Title.js
@@ -4,7 +4,7 @@ export default function Title() {
 	return (
 		<>
 			<div id='title'>
-				<img src='%PUBLIC_URL%/icon.svg' id='icon'></img>
+				<img src='./icon.svg' id='icon'></img>
 				<h1>aaasdad</h1>
 			</div>
 		</>
